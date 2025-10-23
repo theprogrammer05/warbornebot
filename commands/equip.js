3f157f1fa68208fb27748fdc73ff9b2c4fee4433
Ash <dankha90@yahoo.com>
2025-10-23 12:54:29 -0500

    fix desc

--- commands/equip.js
+++ commands/equip.js
@@ -2,10 +2,11 @@ export default {
     name: 'equip',
     description: 'Shows equipment tier values.',
     execute(interaction) {
-        const response = `Tier 8: 36,400
-Tier 9: 47,300
-Tier 10: 61,500
-Tier 11: 79,900`;
+        const response = `
+        Tier 8: 36,400 Exergy
+        Tier 9: 47,300
+        Tier 10: 61,500
+        Tier 11: 79,900`;
 
         interaction.reply(response);
     }
