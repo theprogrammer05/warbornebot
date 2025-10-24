5463877646d566d611a5908cc547e89c23192419
Ash <dankha90@yahoo.com>
2025-10-23 19:03:49 -0500

    fix desc

---
+++ commands/kyky.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'driftmark',
+    description: 'Shows Driftmark tiers and Exergy/Starfall values.',
+    execute(interaction) {
+        const response = `Literal clown.`;
+
+        interaction.reply(response);
+    }
+};
